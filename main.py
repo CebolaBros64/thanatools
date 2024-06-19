630332a78651fa3e117589a939cbe0fd23f6a8d8
Vinny <15020325+CebolaBros64@users.noreply.github.com.>
2024-06-19 13:01:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ if __name__ == '__main__':
     print("=== THANATOOLS ===")
 
     with open('panepon.toml', 'rb') as toml:
-        with open('character_bios.bin', 'rb') as rom:
-            test = Block(toml, rom)
+        # with open('character_bios.bin', 'rb') as rom:
+        #     test = Block(toml, rom)
+
+        with open('games/Panel de Pon (World) (Ja) (Rev 1) (Virtual Console, Switch Online).sfc', 'rb') as rom:
+            test = Block(toml, rom, 0x51637, 0x796)
 
     for i, message in enumerate(test.messages):
         print(f"= Message {i} =")
