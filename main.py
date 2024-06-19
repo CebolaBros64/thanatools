8faf8c2c1041bcd15392175c0a3af210f3d38a38
Vinny <15020325+CebolaBros64@users.noreply.github.com.>
2024-06-19 09:36:03 -0300

    Move the rest of the code to the module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from panepon.script import Block, Text, ControlCode
+from panepon.script import Block
 
 if __name__ == '__main__':
     print("=== THANATOOLS ===")
@@ -7,44 +7,10 @@ if __name__ == '__main__':
     with open('panepon.toml', 'rb') as toml:
         with open('character_bios.bin', 'rb') as rom:
             test = Block(toml, rom)
-    
-    binMessages = test.bytes.split(b'\xff')
-    messages = []
-    
-    for binMessage in binMessages:
-        message = []
-        section = b''
-        i = 0
 
-        while i < len(binMessage):
-            byte = binMessage[i]
+    for i, message in enumerate(test.messages):
+        print(f"= Message {i} =")
 
-            if byte >= 0xF1 and f"{byte:02x}".upper() in test.game['encoding']['controls']:
-                ctrlCode = test.game['encoding']['controls'][f"{byte:02x}".upper()]
-
-                if section != b'':
-                    message.append(Text(section))
-                    section = b''
-
-                if 'length' in ctrlCode:
-                    message.append(ControlCode(binMessage[i:i+ctrlCode['length']+1]))
-                    i += ctrlCode['length'] + 1
-                else:
-                    section += byte.to_bytes()
-                    i += 1
-            else:
-                section += byte.to_bytes()
-                i += 1
-
-        messages.append(message)
-
-    # pprint(messages)
-    for i, message in enumerate(messages):
-        print(f"Message {i}")
-
-        for section in message:
-            print(section.as_bytes())
+        for i2, section in enumerate(message):
+            print(f"Section {i2:02}: {section.as_bytes()}")
             # print(section.as_text())
-        
-
-    
--- panepon/script/__init__.py
+++ panepon/script/__init__.py
@@ -1,12 +1,5 @@
 import tomllib
 
-class Block:
-    def __init__(self, game_toml, rom, length=None, start_offset=0):
-        self.game = tomllib.load(game_toml)['game']
-
-        rom.seek(start_offset)
-        self.bytes = rom.read(length)
-
 class Section:
     def __init__(self, bytes):
         self.bytes = bytes
@@ -21,3 +14,40 @@ class Text(Section):
 class ControlCode(Section):
     def as_text(self):
         pass
+
+class Block:
+    def __init__(self, game_toml, rom, length=None, start_offset=0):
+        self.game = tomllib.load(game_toml)['game']
+
+        rom.seek(start_offset)
+        self.bytes = rom.read(length)
+
+        binMessages = self.bytes.split(b'\xff')
+        self.messages = []
+        
+        for binMessage in binMessages:
+            message = []
+            section = b''
+            i = 0
+
+            while i < len(binMessage):
+                byte = binMessage[i]
+
+                if byte >= 0xF1 and f"{byte:02x}".upper() in self.game['encoding']['controls']:
+                    ctrlCode = self.game['encoding']['controls'][f"{byte:02x}".upper()]
+
+                    if section != b'':
+                        message.append(Text(section))
+                        section = b''
+
+                    if 'length' in ctrlCode:
+                        message.append(ControlCode(binMessage[i:i+ctrlCode['length']+1]))
+                        i += ctrlCode['length'] + 1
+                    else:
+                        section += byte.to_bytes()
+                        i += 1
+                else:
+                    section += byte.to_bytes()
+                    i += 1
+
+            self.messages.append(message)
