7fd0c33d7de77e13e75581e02c5d96f59b1650cc
Vinny <15020325+CebolaBros64@users.noreply.github.com.>
2024-06-20 15:54:20 -0300

    Add tetris attack test to main.py

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
         with open('games/Panel de Pon (World) (Ja) (Rev 1) (Virtual Console, Switch Online).sfc', 'rb') as rom:
             test = Block(toml, rom, 0x51637, 0x796)
 
+    # with open('tattack.toml', 'rb') as toml:
+    #     with open('games/Tetris Attack (USA) (En,Ja).sfc', 'rb') as rom:
+    #         test = Block(toml, rom, 0xCAE5B, 0xA2E)
+
     for i, message in enumerate(test.messages):
+        txtMessage = str(message)
         print()
         print(f"= Message {i} =")
-        print(message)
+        print(txtMessage)
